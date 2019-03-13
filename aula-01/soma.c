c5a7dcd6ec34685f876c7360ca2bdb9fd521b873
MARCO ANTONIO BARAO NEVES <marco.abneves@senacsp.edu.br>
2019-03-12 22:21:36 -0300

    Finalizando a primeira aula

---
+++ aula-01/soma.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declaro as variáveis que o programa utilizará
+    int numero1, numero2, soma;
+
+    printf("Digite um valor:\n");
+    scanf("%d", &numero1);
+
+    
+    printf("Digite outro valor:\n");
+    scanf("%d", &numero2);
+
+    soma = numero1 + numero2;
+
+    printf("O resultado da soma eh: %d", soma);
+
+    return 0;
+}
---
+++ aula-01/teste.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Essa é a função principal 
+        do meu programa */
+int main()
+{
+    // Essa função exibe uma mensagem na tela
+    printf("Bem vindos ao C!\n");
+    printf("Teste");
+
+    return 0;
+}
